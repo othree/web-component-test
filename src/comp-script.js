865a3daeb59a971565bc5d79a69a5a3ef87f4e74
othree_kao <othree_kao@htc.com>
2013-11-18 19:02:52 +0800

    Style

--- src/comp-script.js
+++ src/comp-script.js
@@ -5,6 +5,7 @@ var wthProto = Object.create(HTMLElement.prototype);
 var tpl = doc.getElementById('wth-template');
 
 wthProto.createdCallback = function () {
+    var root = this;
     var host = this.webkitCreateShadowRoot();
     var clone = tpl.content.cloneNode(true);
 
@@ -14,21 +15,32 @@ wthProto.createdCallback = function () {
     var hell = host.querySelector('.hell');
     var toggle = function () {
         if (what.classList.contains('active')) {
-            what.classList.remove('active')
-            hell.classList.remove('active')
-
+            hide();
         } else {
-            what.classList.add('active')
-            hell.classList.add('active')
-
-            hell.style.top = this.offsetTop + what.offsetHeight + 'px';
-            hell.style.left = this.offsetLeft + 'px';
+            show();
         }
+    };
+
+    var show = function () {
+        what.classList.add('active')
+        hell.classList.add('active')
+
+        hell.style.top = (root.offsetTop + what.offsetHeight + 1) + 'px';
+        hell.style.left = root.offsetLeft + 'px';
+    };
+    var hide = function () {
+        what.classList.remove('active')
+        hell.classList.remove('active')
     };
 
     this.onclick = function (event) {
         this.value = event.target.getAttribute('value');
         toggle();
+        event.stopPropagation()
+    };
+
+    document.onclick = function () {
+        hide();
     };
 };
 
